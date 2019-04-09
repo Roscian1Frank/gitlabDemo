403a3f3fa7a5c7e33384ebeaac89b435486b9d14
Roscian Frank <roscian.frank@gmail.com>
2019-04-09 12:00:24 +0100

    update in backend

--- src/main/java/backEnd/BE.java
+++ src/main/java/backEnd/BE.java
@@ -3,6 +3,6 @@ package backEnd;
 public class BE {
     public static String name = "Backend";
     public static void printBE() {
-        System.out.println("Hello from " +name);
+        System.out.println("Updated " +name);
     }
 }
